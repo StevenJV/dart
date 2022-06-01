ca4e076ceac4bc8dde39f32498da62f6fa8465d6
StevenJV <svore@navexglobal.com>
2022-06-01 11:34:27 -0400

    working with non-certificate server

--- Program.cs
+++ Program.cs
@@ -5,24 +5,30 @@ using OpenQA.Selenium.Chrome;
 namespace VSCode {
 [TestFixture]     
 class Program {
-     private IWebDriver driver {get; set;}
-     //private String _url = "http://www.jtable.org/Demo/Filtering";         
-     private String _url = "https://dna01.dev.insights.local";         
+     private IWebDriver driver {get; set;} = null!;
+     private String _url = null!;         
+     private String _search = null!;
+     private String _expected = null!;
  
 [SetUp]
 public void SetupTest()
 {                   
-    //Configuration to Windows
-    //  String driverPath = @"D:\Projetos\VsCode\SeleniumDotNetCore\bin\Debug\netcoreapp2.1";
-    //  String driverExecutableFileName = "chromedriver.exe";         
-    //  ChromeOptions options = new ChromeOptions();
-    //  options.AddArguments("window-size=1200x600");      
-    //Configuration to Linux - Container
+  
+    _url = "https://dm1.in.navex-pe.com/";    // wobbly     
+    _search = "//a[@id=\"ForgotPassword\"]";
+    _expected = "Forgot Password";
+
+    // _url = "https://steven.vorefamily.net/"; 
+    // _search = "//h1[@id=\"site-title\"]/a";
+    // _expected = "Steven's Notebook";
+
     String driverPath = "/usr/bin";        
     String driverExecutableFileName = "chromedriver";
     ChromeOptions options = new ChromeOptions();
     options.AddArguments("headless");
     options.AddArguments("no-sandbox");        
+    options.AddArguments("window-size=1920,1080");        
+    options.AddArguments("disable-dev-shm-usage");        
     options.AddArguments("ignore-certificate-errors");
     options.BinaryLocation = "/usr/bin/google-chrome-stable";            
     ChromeDriverService service = ChromeDriverService.CreateDefaultService(driverPath,driverExecutableFileName);         
@@ -40,10 +46,9 @@ public void testMethod()
 {   
     try{
         this.driver.Navigate().GoToUrl(this._url);                                              
-        IWebElement bodyElement = this.driver.FindElement(By.XPath("//div[contains(@class,'main-header')]/h2"));
-        String strContent= bodyElement.GetAttribute("textContent");
-        Assert.AreEqual("A JQuery plugin to create AJAX based CRUD tables." , strContent);
-    
+        IWebElement titleEl = this.driver.FindElement(By.XPath(_search));
+        String title = titleEl.Text;
+        Assert.AreEqual(_expected , title);
     }
     catch (Exception ex)
     {
