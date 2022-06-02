5d7a46cc7306f0dca7486616c59242c1b1f4d75c
StevenJV <svore@navexglobal.com>
2022-06-02 08:16:59 -0400

    arranged for multiple tests

--- Program.cs
+++ Program.cs
@@ -13,23 +13,14 @@ class Program {
 [SetUp]
 public void SetupTest()
 {                   
-  
-    _url = "https://dm1.in.navex-pe.com/";    // wobbly     
-    _search = "//a[@id=\"ForgotPassword\"]";
-    _expected = "Forgot Password";
-
-    // _url = "https://steven.vorefamily.net/"; 
-    // _search = "//h1[@id=\"site-title\"]/a";
-    // _expected = "Steven's Notebook";
-
     String driverPath = "/usr/bin";        
     String driverExecutableFileName = "chromedriver";
     ChromeOptions options = new ChromeOptions();
-    options.AddArguments("headless");
-    options.AddArguments("no-sandbox");        
+    //options.AddArguments("headless");
+    //options.AddArguments("no-sandbox");        
     options.AddArguments("window-size=1920,1080");        
     options.AddArguments("disable-dev-shm-usage");        
-    options.AddArguments("ignore-certificate-errors");
+    //options.AddArguments("ignore-certificate-errors");
     options.BinaryLocation = "/usr/bin/google-chrome-stable";            
     ChromeDriverService service = ChromeDriverService.CreateDefaultService(driverPath,driverExecutableFileName);         
     driver = new ChromeDriver(service,options,TimeSpan.FromSeconds(30));
@@ -42,8 +33,27 @@ public void TeardownTest()
     driver.Quit();
 }
 [Test]
-public void testMethod()
+public void testInsightsWobbly()
+{   _url = "https://dm1.in.navex-pe.com/";    // wobbly     
+    _search = "//a[@id=\"ForgotPassword\"]";
+    _expected = "Forgot Password";
+    try{
+        this.driver.Navigate().GoToUrl(this._url);                                              
+        IWebElement titleEl = this.driver.FindElement(By.XPath(_search));
+        String title = titleEl.Text;
+        Assert.AreEqual(_expected , title);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.ToString());
+    }        
+}
+[Test]
+public void testInsightsBlog()
 {   
+    _url = "https://steven.vorefamily.net/"; 
+    _search = "//h1[@id=\"site-title\"]/a";
+    _expected = "Steven's Notebook";
     try{
         this.driver.Navigate().GoToUrl(this._url);                                              
         IWebElement titleEl = this.driver.FindElement(By.XPath(_search));
